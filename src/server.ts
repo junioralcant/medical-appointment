2f00f408dc2b5e6712a399575db552415a8f1905
junioralcant <junioralcant111@gmail.com>
2022-11-02 16:35:30 -0300

    config swagger

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,19 @@ import express from 'express';
 import { specialityRouter } from './routes/speciality.routes';
 import { userRouter } from './routes/user.routes';
 
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerDocument from '../swagger.json';
+
 const app = express();
 
 app.use(express.json());
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 
 app.use(userRouter);
 app.use(specialityRouter);
