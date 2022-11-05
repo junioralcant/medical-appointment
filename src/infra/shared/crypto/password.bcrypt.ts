7c0b2832f2ee118802fa381b2ab610ba5dbf339f
junioralcant <junioralcant111@gmail.com>
2022-11-05 09:56:41 -0300

    create authenticate use case

--- src/infra/shared/crypto/password.bcrypt.ts
+++ src/infra/shared/crypto/password.bcrypt.ts
@@ -6,4 +6,11 @@ export class PasswordBcrypt implements IPasswordCrypto {
   hasd(password: string): Promise<string> {
     return bcrypt.hash(password, 10);
   }
+
+  async compare(
+    password: string,
+    passwordHash: string
+  ): Promise<boolean> {
+    return bcrypt.compare(password, passwordHash);
+  }
 }
