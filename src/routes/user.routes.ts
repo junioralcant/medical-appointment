44a532146dcee5fc127533073cae57f148df17f6
junioralcant <junioralcant111@gmail.com>
2022-11-05 10:14:42 -0300

    create authenticate controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import { authenticateUserController } from '../modules/users/useCases/authenticate-user';
 import { createUserController } from '../modules/users/useCases/create-user';
 
 const userRouter = Router();
+
+userRouter.post('/login', async (req, res) => {
+  await authenticateUserController.handle(req, res);
+});
 
 userRouter.post('/users', (req, res) => {
   createUserController.handle(req, res);
